5c58472218b519385eb75baef8dd3ce4acce32e0
Subaru Natsuki <rajibsarkar986431@gmail.com>
2026-05-25 22:42:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 # config.py
 
-API_ID = "" #API ID get it from telegram.org
-API_HASH = "" #API HASH get it from telegram.org
-BOT_TOKEN = "" #Bot Token get it from @BotFather
+API_ID = "39407537" #API ID get it from telegram.org
+API_HASH = "5bd2e83dd1227da3f38c966d1d46d9ae" #API HASH get it from telegram.org
+BOT_TOKEN = "8949977049:AAEckifvYl7f0QXKBvusHatuXbGSzk4Kp_4" #Bot Token get it from @BotFather
 URL_A = "https://myanimelist.net/rss/news.xml" #News RSS Feed Url 2
 #URL_B = "" #News RSS Feed Url 2
 START_PIC = "https://images6.alphacoders.com/131/1313754.png" #Startup Pic [Optional]
-MONGO_URI = "" #Databse uri get it from https://www.mongodb.com/
-ADMINS = [7086472788]
+MONGO_URI = "mongodb+srv://ADMIN98:Irfan987065@cluster0.eqcgcaq.mongodb.net/?appName=Cluster0" #Databse uri get it from https://www.mongodb.com/
+ADMINS = [8180269769]
